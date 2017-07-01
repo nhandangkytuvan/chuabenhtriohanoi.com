21f257494b7596a5c2048ffcf777fdeeefe702ae
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-07-01 10:07:42 +0700

    ok

--- resources/views/layouts/bannerDesktop.blade.php
+++ resources/views/layouts/bannerDesktop.blade.php
@@ -1,5 +1,16 @@
 <div class="myBanner">
 	<div>
-		<a href="#"><img src="{{ asset('public/css/desktop/imghome/home-3.png') }}" alt="" class="center-block"></a>
+		<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en"><img src="{{ asset('public/css/desktop/imghome/home-3.png') }}" alt="" class="center-block"></a>
+	</div>
+	<div>
+		<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en"><img src="{{ asset('public/css/desktop/imghome/banner1.png') }}" alt="" class="center-block"></a>
 	</div>
 </div>
+<script>
+	$('.myBanner').slick({
+	  	slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	autoplay: true,
+	  	autoplaySpeed: 2000,
+	});	
+</script>
