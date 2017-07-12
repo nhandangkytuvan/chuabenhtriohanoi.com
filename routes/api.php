de6fba3e7130c0487d77a83e58980d5c22b27a61
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-07-12 13:50:02 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('term/{id}',function($id){
+	$posts = App\Post::select('id','name','description')->whereTermId($id)->limit(6)->get();
+	foreach ($posts as $key => $post) {
+		$post->description = APIPost::getDescription($post->description,22).'...';
+	}
+	return json_encode($posts);
+});
+// show post
+Route::get('post/{id}',function($id){
+	$post = App\Post::find($id);
+	return json_encode($post);
+});
